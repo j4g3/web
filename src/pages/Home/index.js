2490cf1e70dd1f936a3b405b03bd4b3e398c0646
João Guilherme <j4g3dev@gmail.com>
2020-07-17 18:12:32 -0300

    Update: Add system of questions and points

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,43 +1,98 @@
 import React, { useState } from "react";
-import { Container, QuestionSpan, ButtonsQuestion, ButtonsBox } from "./style";
+import {
+  Container,
+  QuestionSpan,
+  ButtonsQuestion,
+  ButtonsBox,
+  EndText,
+  PointsText,
+} from "./style";
 
 export default function Home() {
   const [Questions] = useState([
     "Tudo bem compartilhar objetos, tipo o celular, pois o novo coronavírus não se transmite dessa forma.",
     "Não preciso me preocupar em higienizar as compras do mercado, afinal, eu mesmo peguei da prateleira com as mãos limpas.",
+    "Os sintomas mais comuns da COVID-19 são: febre, tosse seca e cansaço.",
     "Quando tossir, a melhor coisa a se fazer é cobrir a boca com a mão, afinal, devo evitar contaminar outras pessoas.",
     "Se me sinto bem e tomo todos os cuidados, não tem problema abraçar e beijar as pessoas que amo.",
-    "Os sintomas mais comuns da COVID-19 são: febre, tosse seca e cansaço.",
-    "É possível estar com a COVID-19 por até 14 dias antes de apresentar os sintomas.",
     "Fazer gargarejo com água morna, sal e vinagre previne infecção.",
     "Usando máscara não é possível ser infectado.",
     "A COVID-19 é mais perigosa para indivíduos acima dos 60 anos ou debilitados, portadores de doenças crônicas e crianças.",
+    "É possível estar com a COVID-19 por até 14 dias antes de apresentar os sintomas.",
     "O novo coronavírus não afeta crianças.",
     "Devo procurar o hospital quando estiver espirrando.",
     "É melhor evitar ir ao hospital, mesmo que pra controlar doenças crônicas, e diminuir as chances de exposição ao vírus.",
   ]);
   const [Response] = useState([
-    "Fake",
-    "Fake",
-    "Fake",
-    "Fake",
-    "Fato",
-    "Fato",
-    "Fato",
-    "Fake",
-    "Fake",
-    "Fake",
-    "Fake",
-    "Fake",
+    "False",
+    "False",
+    "True",
+    "False",
+    "False",
+    "False",
+    "True",
+    "False",
+    "True",
+    "False",
+    "False",
+    "False",
   ]);
+  const [Options] = useState(["Real", "Falso"]);
+  let [points, setPoints] = useState(0);
+  function Validator(QuestionID, answer) {
+    // eslint-disable-next-line
+    switch (answer) {
+      case "Real":
+        if (Response[QuestionID]) {
+          if (Response[QuestionID] === "True") {
+            setPoints((points += 1));
+          } else {
+            if (points <= 0) {
+              setPoints(0);
+            } else {
+              setPoints((points -= 1));
+            }
+          }
+          break;
+        }
+      // eslint-disable-next-line
+      case "Falso":
+        if (Response[QuestionID]) {
+          if (Response[QuestionID] === "False") {
+            setPoints((points += 1));
+          } else {
+            if (points <= 0) {
+              setPoints(0);
+            } else {
+              setPoints((points -= 1));
+            }
+          }
+          break;
+        }
+    }
+  }
 
   return (
     <Container>
-      <QuestionSpan>{Questions[0]}</QuestionSpan>
-      <ButtonsBox>
-        <ButtonsQuestion>Real</ButtonsQuestion>
-        <ButtonsQuestion>Falso</ButtonsQuestion>
-      </ButtonsBox>
+      {points < 12 && (
+        <>
+          <PointsText>Pontos - {points}</PointsText>
+          <QuestionSpan>{Questions[points]}</QuestionSpan>
+          <ButtonsBox>
+            {Options.map((i) => (
+              <ButtonsQuestion
+                key={i}
+                onClick={() => {
+                  Validator(points, i);
+                }}
+              >
+                {i}
+              </ButtonsQuestion>
+            ))}
+          </ButtonsBox>
+        </>
+      )}
+      {points >= 12 && <EndText>Parabéns você chegou ao final!</EndText>}
     </Container>
   );
 }
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { darken } from "polished";
 
 export const Container = styled.div`
   position: absolute;
@@ -23,16 +24,31 @@ export const ButtonsBox = styled.div`
   padding: 2%;
   grid-template-columns: 1fr 1fr;
   grid-gap: 10px;
+  padding: 5%;
 `;
 export const ButtonsQuestion = styled.button`
-  background: #bdb2ff;
-  border: 1px solid #bdb2ff;
+  background: ${darken(0.2, "#bdb2ff")};
+  border: 1px solid ${darken(0.2, "#bdb2ff")};
   padding: 3%;
-  box-shadow: 0px 0px 1px 1px #bdb2ff;
+  box-shadow: 0px 0px 1px 1px ${darken(0.2, "#bdb2ff")};
   transition: 1s;
   text-transform: uppercase;
   color: #fff;
+  font-weight: bold;
   &:hover {
-    background: none;
+    color: ${darken(0.2, "#bdb2ff")};
+    background: #bdb2ff;
+    box-shadow: 0px 0px 1px 1px #bdb2ff;
+    border: 1px solid #bdb2ff;
   }
+`;
+export const PointsText = styled.span`
+  font-size: 110%;
+  font-weight: bold;
+  padding: 5%;
+  text-transform: uppercase;
+`;
+export const EndText = styled.span`
+  font-weight: bold;
+  font-size: 150%;
 `;
