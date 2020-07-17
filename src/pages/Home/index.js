185d07233ed1238eab0d144f035a22bcab46b7b8
João Guilherme <j4g3dev@gmail.com>
2020-07-17 16:33:08 -0300

    Update: Add Questions and responses

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,43 @@
-import React from "react";
-import { Container } from "./style";
+import React, { useState } from "react";
+import { Container, QuestionSpan, ButtonsQuestion, ButtonsBox } from "./style";
 
 export default function Home() {
-  return <></>;
+  const [Questions] = useState([
+    "Tudo bem compartilhar objetos, tipo o celular, pois o novo coronavírus não se transmite dessa forma.",
+    "Não preciso me preocupar em higienizar as compras do mercado, afinal, eu mesmo peguei da prateleira com as mãos limpas.",
+    "Quando tossir, a melhor coisa a se fazer é cobrir a boca com a mão, afinal, devo evitar contaminar outras pessoas.",
+    "Se me sinto bem e tomo todos os cuidados, não tem problema abraçar e beijar as pessoas que amo.",
+    "Os sintomas mais comuns da COVID-19 são: febre, tosse seca e cansaço.",
+    "É possível estar com a COVID-19 por até 14 dias antes de apresentar os sintomas.",
+    "Fazer gargarejo com água morna, sal e vinagre previne infecção.",
+    "Usando máscara não é possível ser infectado.",
+    "A COVID-19 é mais perigosa para indivíduos acima dos 60 anos ou debilitados, portadores de doenças crônicas e crianças.",
+    "O novo coronavírus não afeta crianças.",
+    "Devo procurar o hospital quando estiver espirrando.",
+    "É melhor evitar ir ao hospital, mesmo que pra controlar doenças crônicas, e diminuir as chances de exposição ao vírus.",
+  ]);
+  const [Response] = useState([
+    "Fake",
+    "Fake",
+    "Fake",
+    "Fake",
+    "Fato",
+    "Fato",
+    "Fato",
+    "Fake",
+    "Fake",
+    "Fake",
+    "Fake",
+    "Fake",
+  ]);
+
+  return (
+    <Container>
+      <QuestionSpan>{Questions[0]}</QuestionSpan>
+      <ButtonsBox>
+        <ButtonsQuestion>Real</ButtonsQuestion>
+        <ButtonsQuestion>Falso</ButtonsQuestion>
+      </ButtonsBox>
+    </Container>
+  );
 }
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,6 +1,38 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   display: flex;
-  flex: 1;
+  text-align: center;
+  padding: 2%;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+`;
+
+export const QuestionSpan = styled.span`
+  font-weight: bold;
+  font-size: 120%;
+  text-transform: uppercase;
+`;
+export const ButtonsBox = styled.div`
+  display: grid;
+  padding: 2%;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 10px;
+`;
+export const ButtonsQuestion = styled.button`
+  background: #bdb2ff;
+  border: 1px solid #bdb2ff;
+  padding: 3%;
+  box-shadow: 0px 0px 1px 1px #bdb2ff;
+  transition: 1s;
+  text-transform: uppercase;
+  color: #fff;
+  &:hover {
+    background: none;
+  }
 `;
